f2a1ba3d13ed61928da51da33344a8c45fe96a35
Madhumitha P <madhupr@thoughtworks.com>
2021-12-16 14:56:28 +0530

    [Madhu] Optimizing day 14 extended polymerization - Polymerizing using recursion. Throws stack level too deep even after polymerizing by splitting the polymer into two halves

--- src/day14/day14.rb
+++ src/day14/day14.rb
@@ -7,39 +7,38 @@ class Day14
     insertion_rules = {}
     input_array.each { |insertion_rule|
       insertion_rule_pair = insertion_rule.split(" -> ")
-      insertion_rules[insertion_rule_pair[0].bytes.join.to_i] = insertion_rule_pair[1].ord
-      # insertion_rules[insertion_rule_pair[0]] = insertion_rule_pair[1]
+      elements = insertion_rule_pair[0].split("")
+      insertion_rules[insertion_rule_pair[0]] = "#{elements[0]}#{insertion_rule_pair[1]}"
     }
     insertion_rules
   end
 
+  def substitute_polymer(polymer, insertion_rules)
+    return insertion_rules[polymer] if polymer.length == 2
+    "#{substitute_polymer(polymer[0..1], insertion_rules)}#{substitute_polymer(polymer[1...polymer.length], insertion_rules)}"
+  end
+
   def polymerize(polymer_template, insertion_rules)
-    polymer_elements = polymer_template.bytes
-    # polymer_elements = polymer_template.split("")
     (0...NO_OF_ITERATIONS).each { |iteration|
       puts "----------- Executing Iteration #{iteration}"
-      polymerization_result = []
-      no_of_polymer_elements = polymer_elements.length
-      (0...(no_of_polymer_elements - 1)).each { |index|
-        insertion_element = insertion_rules[polymer_elements[index..(index + 1)].join.to_i]
-        # insertion_element = insertion_rules["#{polymer_elements[index]}#{polymer_elements[index + 1]}"]
-        insertion_position = index * 2
-        polymerization_result[insertion_position] = polymer_elements[index]
-        polymerization_result[insertion_position + 1] = insertion_element
-      }
-      polymerization_result[(no_of_polymer_elements - 1) * 2] = polymer_elements[no_of_polymer_elements - 1]
-      polymer_elements = polymerization_result
+      length = polymer_template.length
+      polymerized_result_left = substitute_polymer(polymer_template[0..length/2], insertion_rules)
+      polymerized_result_right = substitute_polymer(polymer_template[length/2...length], insertion_rules)
+      last_element = polymer_template[-1, 1]
+      polymer_template = "#{polymerized_result_left}#{polymerized_result_right}#{last_element}"
     }
-    polymer_elements
+    polymer_template.split("")
   end
 
   def compute
-    input_file_content = FileReader.new.read("day14_sample.txt")
+    puts "Start time: #{Time.now}"
+    input_file_content = FileReader.new.read("day14.txt")
     input_array = input_file_content.split("\n")
     polymer_template = input_array[0]
     insertion_rules = build_insertion_rules(input_array[2, input_array.size])
     polymerized_elements = polymerize(polymer_template, insertion_rules)
     sorted_elements_count = get_sorted_elements_with_count(polymerized_elements)
+    puts "End time: #{Time.now}"
     sorted_elements_count.last - sorted_elements_count.first
   end
 
