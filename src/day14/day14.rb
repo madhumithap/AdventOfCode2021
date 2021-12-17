9e2c3ac70c2ede6156952de17d3d8bf9b5defb17
Madhumitha P <madhupr@thoughtworks.com>
2021-12-17 07:33:45 +0530

    [Madhu] Optimizing day 14 extended polymerization - Finally made it running for 40 iterations without brute force

--- src/day14/day14.rb
+++ src/day14/day14.rb
@@ -1,33 +1,57 @@
 require_relative "../util/file_reader"
 
-NO_OF_ITERATIONS = 10
+NO_OF_ITERATIONS = 40
 
 class Day14
   def build_insertion_rules(input_array)
     insertion_rules = {}
     input_array.each { |insertion_rule|
       insertion_rule_pair = insertion_rule.split(" -> ")
-      elements = insertion_rule_pair[0].split("")
-      insertion_rules[insertion_rule_pair[0]] = "#{elements[0]}#{insertion_rule_pair[1]}"
+      insertion_rules[insertion_rule_pair[0]] = insertion_rule_pair[1]
     }
     insertion_rules
   end
 
-  def substitute_polymer(polymer, insertion_rules)
-    return insertion_rules[polymer] if polymer.length == 2
-    "#{substitute_polymer(polymer[0..1], insertion_rules)}#{substitute_polymer(polymer[1...polymer.length], insertion_rules)}"
+  def build_pairwise_map(polymer_elements)
+    pairwise_map = {}
+    elements_count = {}
+    no_of_polymer_elements = polymer_elements.length
+    (0...(no_of_polymer_elements - 1)).each { |index|
+      formula = "#{polymer_elements[index]}#{polymer_elements[index + 1]}"
+      pairwise_map[formula] = value_of(formula, pairwise_map)
+      elements_count[polymer_elements[index]] = value_of(polymer_elements[index], elements_count)
+    }
+    elements_count[polymer_elements[no_of_polymer_elements - 1]] = value_of(polymer_elements[no_of_polymer_elements - 1], elements_count)
+    puts pairwise_map
+    puts elements_count
+    [pairwise_map, elements_count]
+  end
+
+  def value_of(element, hash)
+    hash.has_key?(element) ? hash[element] + 1 : 1
   end
 
   def polymerize(polymer_template, insertion_rules)
+    polymer_elements = polymer_template.split("")
+    pairwise_map_with_count = build_pairwise_map(polymer_elements)
+    pairwise_map = pairwise_map_with_count[0]
+    elements_count = pairwise_map_with_count[1]
     (0...NO_OF_ITERATIONS).each { |iteration|
-      puts "----------- Executing Iteration #{iteration}"
-      length = polymer_template.length
-      polymerized_result_left = substitute_polymer(polymer_template[0..length/2], insertion_rules)
-      polymerized_result_right = substitute_polymer(polymer_template[length/2...length], insertion_rules)
-      last_element = polymer_template[-1, 1]
-      polymer_template = "#{polymerized_result_left}#{polymerized_result_right}#{last_element}"
+      puts "---------- Executing Iteration #{iteration + 1}"
+      polymerized_map = {}
+      pairwise_map.each { |k, count|
+        insertion_element = insertion_rules[k]
+        elements = k.split("")
+
+        first_polymer = "#{elements[0]}#{insertion_element}"
+        second_polymer = "#{insertion_element}#{elements[1]}"
+        polymerized_map[first_polymer] = value_of(first_polymer, polymerized_map) + (count-1)
+        polymerized_map[second_polymer] = value_of(second_polymer, polymerized_map) + (count-1)
+        elements_count[insertion_element] = value_of(insertion_element, elements_count) + (count-1)
+      }
+      pairwise_map = polymerized_map
     }
-    polymer_template.split("")
+    elements_count
   end
 
   def compute
@@ -36,20 +60,10 @@ class Day14
     input_array = input_file_content.split("\n")
     polymer_template = input_array[0]
     insertion_rules = build_insertion_rules(input_array[2, input_array.size])
-    polymerized_elements = polymerize(polymer_template, insertion_rules)
-    sorted_elements_count = get_sorted_elements_with_count(polymerized_elements)
+    polymerized_elements_count = polymerize(polymer_template, insertion_rules)
+    sorted_elements_count = polymerized_elements_count.values.sort
     puts "End time: #{Time.now}"
     sorted_elements_count.last - sorted_elements_count.first
-  end
-
-  private
-
-  def get_sorted_elements_with_count(polymerized_elements)
-    elements_count = {}
-    polymerized_elements.each { |element|
-      elements_count[element] = elements_count.has_key?(element) ? elements_count[element] + 1 : 0
-    }
-    elements_count.values.sort
   end
 end
 
